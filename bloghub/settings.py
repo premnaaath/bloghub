601e3f38aa24ce3393fe725c29130008a26d1433
Premnaath <premnaathvaradharajan@gmail.com>
2021-07-23 17:29:44 +0530

    Update settings.py

--- bloghub/settings.py
+++ bloghub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n(6np!ameknoo)1!(_)a56=%oe7=+ysk+*sy_&)yi0ig&rn48&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bloghub-django.herokuapp.com/',
+ALLOWED_HOSTS = ['bloghub-django.herokuapp.com',
                  '127.0.0.1']
 
 
