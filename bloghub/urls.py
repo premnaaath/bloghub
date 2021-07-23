f5a7c288498b2b38b37a807c72c1a7bf19377cd5
Premnaath <premnaathvaradharajan@gmail.com>
2021-07-23 18:57:19 +0530

    Update settings.py

--- bloghub/urls.py
+++ bloghub/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('register/', users_view.register,  name='register'),
     path('login/', auth_view.LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name="users/logout.html"),  name='logout'),
