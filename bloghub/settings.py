de30d2238338413cef851375ce2b7849934f7d72
Premnaath <premnaathvaradharajan@gmail.com>
2021-07-23 17:25:53 +0530

    Update settings.py

--- bloghub/settings.py
+++ bloghub/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'acff963a4bc07ecc6ddfa29efbbe8315532328d3e360401d'
+SECRET_KEY = 'django-insecure-n(6np!ameknoo)1!(_)a56=%oe7=+ysk+*sy_&)yi0ig&rn48&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://bloghub-django.herokuapp.com/']
+ALLOWED_HOSTS = ['https://bloghub-django.herokuapp.com/',
+                 '127.0.0.1']
 
 
 # Application definition
